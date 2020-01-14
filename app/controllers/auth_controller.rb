cb26b2185ea82fbe9a6fd2118015c8df2fecf337
mazenswar <mazenswar@protonmail.com>
2020-01-14 16:23:24 -0500

    v1

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,21 @@
+class AuthController < ApplicationController
+    
+    def login
+        user = User.find_by(username: user_params['username'])
+        if user && user.authenticate(user_params['password'])
+            render json: {user: user, token: encode_token(user)}
+        else 
+            render json: {errors:  user.errors.full_messages}
+        end
+    end
+
+    def persist
+        if token
+            render json: logged_in_user
+        end
+    end
+
+     def user_params
+        params.permit(:username, :password)
+    end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+
+    def create
+        user = User.create(user_params)
+        if user.valid? 
+            render json: {user: user, token: encode_token(user)}
+        else
+            render json: {errors:  user.errors.full_messages}
+        end
+    end
+
+    private 
+
+    def user_params
+        params.permit(:username, :password)
+    end
+end
